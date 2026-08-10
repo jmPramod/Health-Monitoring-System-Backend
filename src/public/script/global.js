c76f473e6cc42a5868d25b943fa5274348c13f0b
Pramod <pramodjm4@gmail.com>
2026-08-10 10:44:53 +0530

    added swagger api docs for patienr and edited handele bars.

--- src/public/script/global.js
+++ src/public/script/global.js
@@ -1,15 +1,13 @@
 const open_model = document.getElementById("modelOpen");
 const model_container = document.getElementById("model-container");
 const close_model = document.getElementById("modelClose");
-console.log(open_model, model_container, close_model);
 
-open_model.addEventListener("click", function () {
-  console.log("open", model_container.classList);
-
-  model_container.classList.add("show");
+open_model.addEventListener("click", () => {
+  model_container.classList.remove("hidden");
+  model_container.classList.add("flex");
 });
 
-close_model.addEventListener("click", function () {
-  console.log("close");
-  model_container.classList.remove("show");
+close_model.addEventListener("click", () => {
+  model_container.classList.remove("flex");
+  model_container.classList.add("hidden");
 });
